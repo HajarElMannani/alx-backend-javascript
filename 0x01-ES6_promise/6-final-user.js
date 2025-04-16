5c4f2d0c96f522f3c0d89314c0281eb98586274e
HajarElMannani <laso125@hotmail.com>
2025-04-16 20:20:09 +0000

    Modification

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,11 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const arr = [];
-      results.map((result) => {
-        if (result.status === 'fulfilled') {
-          arr.push({ status: result.status, value: result.value });
-        } else {
-          arr.push({ status: result.status, value: result.reason });
-        }
-        return arr;
-      });
-    });
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(results => results.map(result => {
+    return result.status === 'fulfilled'
+      ? { status: result.status, value: result.value }
+      : { status: result.status, value: result.reason };
+  }));
 }
