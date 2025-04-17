28343466eccbf9d34d4fa678f32db6e221273668
HajarElMannani <laso125@hotmail.com>
2025-04-17 20:31:58 +0000

    Fix eslint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,40 @@
 export default class ALXCourse {
-    constructor(name, length, students) {
-	    this._name = name;
-	    this._length = length;
-	    this._students = students;
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get name() {
-	return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(name) {
-	if (typeof(name) !== 'string'){
-            throw new TypeError('Name must be a string');
-	}
-	this._name = name;
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    
-    get	length() {
-        return this._length;
-    }
+    this._name = name;
+  }
 
-    set	length(length) {
-	if (typeof(length) !== 'number'){
-            throw new TypeError('Length must be a number');
-        }
-        this._length = length;
-    }
+  get length() {
+    return this._length;
+  }
 
-    get	students() {
-        return this._students;
+  set length(length) {
+    if (typeof (length) !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
+  }
 
-    set	students(students) {
-	if (!Array.isArray(students)){
-            throw new TypeError('Students must be an array');
-        }
-        this._students = students;
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array');
     }
-
+    this._students = students;
+  }
 }
