091e7123e597208915db0e10aa4e16e7b5659023
HajarElMannani <laso125@hotmail.com>
2025-04-21 21:06:11 +0000

    First commit

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+  return '';
+  }
+  const result = [];
+  for (const elem of set) {
+    if (elem && elem.startsWith(startString)) {
+      result.push(elem.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
