06febc6cc1e900c2b16f4667855d344fbb4c2574
HajarElMannani <laso125@hotmail.com>
2025-04-23 16:24:58 +0000

    Task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -38,14 +38,14 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee(salary: number | string)): (Director | Teacher) {
+function createEmployee(salary: number | string): (Director | Teacher) {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher) {
+function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
@@ -54,5 +54,15 @@ function executeWork(employee: Director | Teacher): string {
     return employee.workDirectorTasks();
   } else {
     return employee.workTeacherTasks();
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
   }
 }
