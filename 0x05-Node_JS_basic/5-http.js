9b9df4eb446e3b03c88cac2837775ca17b170b02
HajarElMannani <laso125@hotmail.com>
2025-06-10 15:25:37 +0000

    Initial commit

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,36 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
-const fileName = process.argv[2]
+const fs = require('fs').promises;
+
+const fileName = process.argv[2];
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+	const data = await fs.readFile(path, 'utf-8');
+	const students = data.trim().split('\n').filter((line) => line.trim() !== '');
+	const lines = students.slice(1);
+	const fields = {};
+	for (const line of lines) {
+	    const seg = line.split(',');
+	    const firstname = seg[0];
+	    const field = seg[3];
+	    if (!fields[field]) {
+		fields[field] = [];
+	    }
+	    fields[field].push(firstname);
+	}
+	console.log(`Number of students: ${lines.length}`);
+	for (const field in fields) {
+	    if (Object.prototype.hasOwnProperty.call(fields, field)) {
+		const list = fields[field].join(', ');
+		console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+	    }
+	}
+    } catch (err) {
+	throw new Error('Cannot load the database');
+    }
+}
+module.exports = countStudents;
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello ALX!');
