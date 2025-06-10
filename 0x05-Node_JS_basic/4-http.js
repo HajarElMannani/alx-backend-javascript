c337aa4aed957f3e4ece6417905a07fe139ba5b5
HajarElMannani <laso125@hotmail.com>
2025-06-10 13:24:59 +0000

    Initial commit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
-const app = module.exports = express();
+const app = express();
 app.use((req, res) => {
   res.send('Hello ALX!');
 });
-app.listen(1245);
+app.listen(1245, () => {
+    console.log('listening on port 1245');
+});
+module.exports = app;
