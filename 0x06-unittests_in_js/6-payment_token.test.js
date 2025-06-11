a0f15d22292a9f64be57f4449ed49ffccbd19805
HajarElMannani <laso125@hotmail.com>
2025-06-11 17:42:14 +0000

    Initial commit

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('should return a resolved promise', function (done) {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.have.property('data', 'Successful response from the API');
+      done();
+    })
+      .catch(done);
+  });
+});
