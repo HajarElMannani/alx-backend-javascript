facaf71e3b34dbbc8f706f27a3653601b4579f45
HajarElMannani <laso125@hotmail.com>
2025-06-12 14:22:27 +0000

    fix Typo

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-app.get('/cart/:id(\\:d+)', (req, res) => {
+app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 app.listen(7865, () => {
