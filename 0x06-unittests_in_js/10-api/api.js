3b828d4f9f4bd22dd2fe07a249ee6e0e01d2deed
HajarElMannani <laso125@hotmail.com>
2025-06-12 20:22:57 +0000

    modify output

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -17,7 +17,7 @@ app.get('/available_payments', (req, res) => {
 });
 app.post('/login', (req, res) => {
   const { userName } = req.body;
-  res.send(`Welcome : ${userName}`);
+  res.send(`Welcome ${userName}`);
 });
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
